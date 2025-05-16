29fe2e5f938049aa1a9fc82e29ce0d9015e53a5a
Noeren <noeren8@gmail.com>
2025-05-16 21:57:53 +0700

    feat: first task complete, sum function created

--- 01-intro/1-sum/index.js
+++ 01-intro/1-sum/index.js
@@ -1,10 +1,12 @@
 /**
  * Sum of two numbers
+ * 
+ * By Ivan Silantev
  *
  * @param {number} m first number
  * @param {number} n second number
  * @returns {number}
  */
 export const sum = (m, n) => {
-
+    return n+m
 };
