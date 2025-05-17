84737bfe97fc6fba28da41e6cf6e52a945f9f8a2
Noeren <noeren8@gmail.com>
2025-05-17 14:12:41 +0700

    feat: task 02-02 complete, pick function created

--- 02-javascript-data-types/2-pick/index.js
+++ 02-javascript-data-types/2-pick/index.js
@@ -1,9 +1,17 @@
 /**
  * pick - Creates an object composed of the picked object properties:
+ * 
+ * By Ivan Silantev
+ * 
  * @param {object} obj - the source object
  * @param {...string} fields - the properties paths to pick
  * @returns {object} - returns the new object
  */
 export const pick = (obj, ...fields) => {
-
+  return fields.reduce((result, field) => {
+    if (field in obj) {
+      result[field] = obj[field];
+    }
+    return result;
+  }, {});
 };
